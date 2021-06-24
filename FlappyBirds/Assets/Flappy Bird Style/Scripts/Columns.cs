2efedf5840f20a2e7bdc16a24552036cdaca8e76
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2021-06-24 14:51:20 -0300

    1 commit
    Simple Game Flappy Bird, just to Test.

---
+++ FlappyBirds/Assets/Flappy Bird Style/Scripts/Columns.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Columns : MonoBehaviour
+{
+   private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Bird>() != null)
+        {
+            GameController.instance.BirdScored();
+        }
+    }
+}
